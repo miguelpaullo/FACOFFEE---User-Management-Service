626d21adb048a5e596398498976f1b109f0b4fe7
ArthurOrtiz-mp4 <arthur.ortiz@ufms.br>
2026-06-10 19:40:47 -0400

    feat: Configurar Prisma e modelagem User

---
+++ prisma/migrations/20260610225506_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'ACTIVE',
+    "roles" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "deactivatedAt" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
